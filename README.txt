ca06130cf09776e60ac51fc78b5a9662c67fd9dd
ws <ws_dev@163.com>
2021-07-06 11:02:43 +0800

    init

---
+++ README.txt
@@ -0,0 +1,2 @@
+greeter code example from https://github.com/grpc/grpc-dotnet/tree/master/examples/Greeter
+added wpf client in this repo
---
+++ src/Greeter/WpfClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Greet;
+using Grpc.Net.Client;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace WpfClient.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public string Text1 { get => _text1; set => SetProperty(ref _text1, value); }
+
+        private string _text1 = "Origin";
+
+        public MainWindowViewModel()
+        {
+            OnClickButton1 = new DelegateCommand(ClickButton1);
+        }
+
+        public void ClickButton1()
+        {
+            //Text1 = "changed";
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" }).GetAwaiter().GetResult();
+            Text1 = reply.Message;
+        }
+
+        public DelegateCommand OnClickButton1 { get; private set; }
+    }
+}
